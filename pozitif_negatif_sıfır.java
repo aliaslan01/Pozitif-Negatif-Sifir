51f7a0d1069c1b171eb348eee3ffac3ee5d610c3
aliaslan01 <aslanmuhammedali047@gmail.com>
2023-08-02 16:13:50 +0300

    Pozitif-negatif-sifir kod
    Java ile pozitif-negatif-sifir bulan proje

---
+++ pozitif_negatif_sıfır.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vize_hazirlik;
+
+
+public class pozitif_negatif_sıfır {
+    
+    public static void main(String[] args) {
+        
+        int i = 5;
+        
+        if(i==0){
+            System.out.println("sayı sıfırdır");
+        }
+        else if(i>0){
+            System.out.println("sayı pozitiftir");
+        }
+        else{
+            System.out.println("sayı negatiftir");
+        }
+        
+    }
+    
+}
